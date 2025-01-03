bf875263997351b520ab3204a9fd7c045c0a84b8
wayne <309334941@qq.com>
2025-01-03 23:40:38 +0800

    try both original oai and the autogen's oai wrap

--- tests/oai_key_test.py
+++ tests/oai_key_test.py
@@ -33,16 +33,41 @@ def get_openai_response(question):
     return response
 
 
-# Autogen OpenAI code
-def get_openai_response_autogen(question):
-    # using the autogen.OpenAIWrapper class to do similar things 
+# Autogen OpenAI code OpenAIWrapper
+# benifit 1: cach for faster response
+def get_openai_response_autogen(message):
     start_time = datetime.now()
 
     # Load environment variables from .env file
     load_dotenv()
-    config_list=[]
-    client=OpenAIWrapper(os.getenv('openai_api_key'))
-    response=client.create(prompt=question,model="gpt-4o-mini")
+
+    # autogen using the llmconfig  https://docs.ag2.ai/docs/topics/llm_configuration
+    config_list={
+        "config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}],
+    }
+    config_list2=[{
+        "model": "gpt-4",
+        "api_key": os.environ["OPENAI_API_KEY"]
+    }]
+
+    base_config={
+        "model": "gpt-4",
+        "api_key": os.environ["OPENAI_API_KEY"],
+        "cache":None
+    }
+    
+    openai_warp=OpenAIWrapper(config_list=config_list2,**base_config)
+    
+    create_config = {}
+    if isinstance(message, str):
+        create_config['messages'] = [{"role": "user", "content": message}]
+    elif isinstance(message, list):
+        create_config['messages'] = message
+    else:
+        raise ValueError("Message must be either a string or a list of messages")
+
+    response = openai_warp.create(**create_config)
+
     end_time = datetime.now()
     time_taken = (end_time - start_time).total_seconds()
     print(f"Autogen version Time taken to get response: {time_taken} seconds")
@@ -51,6 +76,8 @@ def get_openai_response_autogen(question):
 
 
 
-test_question="What is the capital of France?"
+test_question="What answser of the 1 plus 1 ?"
+print("=================response from Origenal OpenAI======================")
 print(get_openai_response(test_question))
+print('=================response from Autogen OpenAI OpenAIWrapper ======================')
 print(get_openai_response_autogen(test_question))
